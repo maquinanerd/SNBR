db533b7e9139304035ed5839f232cfe44802ff03
maquinanerd <30965113+maquinanerd@users.noreply.github.com>
2025-10-01 15:28:06 -0300

    ALTERÇÕES QUE EU PEDI

--- app/cleaners.py
+++ app/cleaners.py
@@ -11,3 +11,20 @@ def clean_html_for_globo_esporte(soup: BeautifulSoup) -> BeautifulSoup:
         player.decompose()
         
     return soup
+
+def clean_html_for_lance(soup: BeautifulSoup) -> BeautifulSoup:
+    """
+    Limpa o HTML de uma página do Lance!, removendo elementos indesejados
+    antes da extração principal de conteúdo.
+    """
+    # Remove o SVG de carregamento que às vezes é incluído como uma imagem
+    for figure in soup.find_all('figure'):
+        if figure.find('img', src=lambda s: s and 'dotsInCircle.svg' in s):
+            figure.decompose()
+            
+    # Remove iframes de publicidade ou outros embeds não relacionados a vídeo
+    for iframe in soup.find_all('iframe'):
+        if 'youtube.com' not in iframe.get('src', ''):
+            iframe.decompose()
+            
+    return soup
